285299330d19f4dc008e21fbb9918406c1dd471a
Buckminsterfullerene02 <bobby45900@gmail.com>
2023-07-03 10:37:40 +0100

    Project files

---
+++ AssetGen/AssetGenerator.bat
@@ -0,0 +1,1 @@
+"F:\UNEPIC GAMES\UE_4.27\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "F:\DRG Modding\Project Generator\FSDTemplateU38P3\FSD.uproject" -run=AssetGenerator -DumpDirectory="F:\DRG Modding\DRGPacker\JSON\Assets" -AssetClassWhitelist="SkeletalMesh" -abslog="F:\DRG Modding\DRGPacker\JSON\AssetGen\AssetGenerator.log" -stdout -unattended -NoLogTimes
